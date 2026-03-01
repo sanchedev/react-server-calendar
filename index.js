c78cd7610e7c2c3efb1a1b7c434a5ce83f994e24
Sanchedev <sanchedev@outlook.com>
2026-02-28 21:47:01 -0500

    Add favicon.svg

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const router = require('./dist/server.js').default
 app.use(express.static('public/'))
 app.use(router)
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000')
-})
+// app.listen(3000, () => {
+//   console.log('http://localhost:3000')
+// })
 
-// module.exports = app // To use with vercel
+module.exports = app // To use with vercel
--- src/ui/app.tsx
+++ src/ui/app.tsx
@@ -12,6 +12,7 @@ export default function App({ lang, year }: AppProps) {
         <meta charSet='UTF-8' />
         <meta name='viewport' content='width=device-width, initial-scale=1.0' />
         <title>React Server Calendar</title>
+        <link rel='shortcut icon' href='/favicon.svg' type='image/svg' />
         <link rel='stylesheet' href='/style.css' />
       </head>
       <body>
